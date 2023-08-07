95b2cb50cf399217d22efd566bddf140e8cc2857
Victor Angelo Legat Cerqueira <91611155+Brenaki@users.noreply.github.com>
2023-08-07 19:08:15 -0300

    API The End - v0.1.1

---
+++ APIEnd/src/services/Environment.ts
@@ -0,0 +1,96 @@
+const en = [
+    {
+        biomes: {
+         // Biomes
+                '1': 'The End',
+                '2': 'Small End islands',
+                '3': 'End midlands',
+                '4': 'End highlands',
+                '5': 'End barrens',
+        },
+        generated_structured: {
+            // Generated Structures
+            '1': 'Central island',
+            '2': 'Outer islands',
+            '3': 'End spike',
+            '4': 'Obsidian plataform',
+            '5': 'Exit portal',
+            '6': 'End gateway',
+            '7': 'End city',
+            '8': 'End ship',
+            '9': 'Chorus tree'
+        },
+        terrain_features: {
+            // Terrain Features
+            'Main_island': `On the main island, there are ten obsidian towers with Ender crystals on '
+                           'top, which regenerate the dragon\'s health. The player must destroy the '
+                           'crystals to weaken the dragon and be able to attack it. When the dragon '
+                           'is defeated, it explodes and drops a lot of experience and a dragon egg. '
+                           'The exit portal is activated and allows the player to return to the '
+                           'normal world or explore the outer islands of the End, where there are '
+                           'cities of the End and ships of the End with treasure and elytra. The '
+                           'dragon can be respawned as many times as the player wants by placing four '
+                           'Ender crystals around the exit portal.`,
+            'Outer_island': `The outer islands of the End are mysterious and isolated places that can '
+                            'only be accessed after defeating the dragon of the End. They are made up '
+                            'of floating islands of varying sizes and shapes, covered with chorus '
+                            'plants that produce chorus fruits. On these islands, the player can find '
+                            'End Cities, which are rare and complex structures that house shulkers '
+                            'and treasure chests. Some End cities have End ships docked near them, '
+                            'which contain even more valuable items, such as elytra and dragon\'s head.`
+        },
+        'mobs': {
+        },
+        'blocks': {
+            //  Blocks
+            '1': {
+                // Naturally generated
+                '1': 'Air',
+                '2': 'Bedrock',
+                '3': 'Obsidian',
+                '4': 'End stone',
+                '5': 'Chorus plant',
+                '6': 'Chorus flower'
+            },
+            '2': {
+                // Naturally created
+                '1': 'Air',
+                '2': 'Bedrock',
+                '3': 'Obsidian',
+                '4': 'Wall torch',
+                '5': 'Fire',
+                '6': 'End portal',
+                '7': 'Dragon egg',
+                '8': 'End gateway',
+                '9': 'End crystal',
+                '10': 'Iron bars',
+                '11': 'End stone'
+            },
+            '3': {
+                // Structures
+                '1': 'Air',
+                '2': 'Obsidian',
+                '3': 'Chest',
+                '4': 'Magenta stained Glass',
+                '5': 'Brewing stand',
+                '6': 'Item frame',
+                '7': 'Magenta wall banner',
+                '8': 'End rod',
+                '9': 'Purpur block',
+                '10': 'Purpur pillar',
+                '11': 'Purpur slab',
+                '12': 'Purpur stairs',
+                '13': 'End stone bricks',
+                '14': 'Dragon wall head',
+                '15': 'Ender chest',
+                '16': 'Ladder',
+            }
+        }
+    }
+]
+
+const pt = [
+    {
+
+    }
+]
